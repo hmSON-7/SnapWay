4cd22b5ded4fd6d7d496d19bf3277fd47ce49ec3
yohan <yohan0924@gamil.com>
2025-12-25 13:29:37 +0900

    final

--- snapway_backend/src/main/java/com/snapway/security/SecurityConfig.java
+++ snapway_backend/src/main/java/com/snapway/security/SecurityConfig.java
@@ -20,19 +20,21 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	@Value("${app.frontServer-origin}")
-    private String frontServerOrigin;
-	@Value("${app.self-origin}")
-    private String selfOrigin;
-	
-	private final JwtUtil jwtUtil;
 
-	private final JwtAuthenticationFilter jwtFilter;
-	public SecurityConfig(JwtUtil jwtUtil, JwtAuthenticationFilter jwtFilter) {
-		this.jwtFilter = jwtFilter;
-		this.jwtUtil = jwtUtil;
-	}
+    @Value("${app.frontServer-origin}")
+    private String frontServerOrigin;
+    @Value("${app.self-origin}")
+    private String selfOrigin;
+
+    private final JwtUtil jwtUtil;
+
+    private final JwtAuthenticationFilter jwtFilter;
+
+    public SecurityConfig(JwtUtil jwtUtil, JwtAuthenticationFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+        this.jwtUtil = jwtUtil;
+    }
+
     // 1. 비밀번호 암호화 빈 등록 (BCrypt)
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -41,54 +43,42 @@ public class SecurityConfig {
 
     // 2. AuthenticationManager 빈 등록 (로그인 로직에서 사용)
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     // 3. Security Filter Chain 설정
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    	http
-    	.csrf(csrf->csrf.disable())
+        http.csrf(csrf -> csrf.disable())
 
-        // CORS
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                // CORS
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
-        // 폼로그인/Basic 비활성화
-        .formLogin(form -> form.disable())
-        .httpBasic(basic -> basic.disable())
+                // 폼로그인/Basic 비활성화
+                .formLogin(form -> form.disable()).httpBasic(basic -> basic.disable())
 
-        // 권한 설정
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers(
-                "/api/member/regist",
-                "/api/member/login",
-                "/api/member/logout",
-                "/api/member/check-email",
-                "/error/**",
-                "/api/member/fetchMyInfo",
-                "/api/csrf",   // csrf 토큰 발급용
-                "/api/article/**",
-                // 테스트를 위한 임시 개방
-                "/api/trip/**",
-                "/api/auth/password/**"
-            ).permitAll()
-            .anyRequest().authenticated()
-        )
-        
-        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                // 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/member/regist", "/api/member/login",
+                                "/api/member/logout", "/api/member/check-email", "/error/**",
+                                "/api/member/fetchMyInfo", "/api/csrf", // csrf 토큰 발급용
+
+                                // 테스트를 위한 임시 개방
+                                "/api/article/**", "/api/trip/**", "/api/auth/password/**")
+                        .permitAll().anyRequest().authenticated())
+
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
     }
-    
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                frontServerOrigin,
-                selfOrigin
-        ));
+        configuration.setAllowedOrigins(List.of(frontServerOrigin, selfOrigin));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
